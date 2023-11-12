dc1416f3c4537bc33fe389a147ff201e382f5923
maehann <104306054+maehann@users.noreply.github.com>
2023-11-12 11:45:12 +0330

    first commit create project and set theme

---
+++ app/src/main/java/com/example/dunipool/CoinActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dunipool
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CoinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coin)
+    }
+}
---
+++ app/src/main/java/com/example/dunipool/MarketActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dunipool
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MarketActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_market)
+    }
+}
