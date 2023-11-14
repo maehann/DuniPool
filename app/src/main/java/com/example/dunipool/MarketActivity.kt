73f63a5e503944f2c00b98eaf0a15b45c6dcb302
maehann <104306054+maehann@users.noreply.github.com>
2023-11-14 10:42:14 +0330

    second commit add toolbar

--- app/src/main/java/com/example/dunipool/MarketActivity.kt
+++ app/src/main/java/com/example/dunipool/MarketActivity.kt
@@ -2,10 +2,14 @@ package com.example.dunipool
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dunipool.databinding.ActivityMarketBinding
 
 class MarketActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMarketBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_market)
+
+        binding=ActivityMarketBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
